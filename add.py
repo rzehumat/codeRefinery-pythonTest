3bd959241505704ed23fa7e58e834b443a9c8cde
Matěj Rzehulka <matej.rzehulka@fykos.cz>
2022-03-31 11:18:30 +0300

    copy from previous example, add subtract

---
+++ add.py
@@ -0,0 +1,22 @@
+from pytest import approx
+
+def add(a, b):
+    return a + b
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
+    
+    result = add(0.1, 0.2)
+    # not good for small values, i.e. if result approx 1e-9
+    assert abs(result-0.3) < 1e-7
+    
+    # more robust
+    assert add(0.1, 0.2) == approx(0.3)
+    # bad example
+    # assert add(0.1, 0.2) == 0.3
+
+
+def subtract(a, b):
+    return a-b
